5e34f08dfdfdf1caed84edd3e19837e304905f90
anjali <yashika22789@gmail.com>
2022-08-31 23:49:31 +0530

    js Q/A

---
+++ if_else_conditionals/question1.js
@@ -0,0 +1,34 @@
+// check if the person is old enough to enter the nightclub(21)
+// log a suitable message to the console in both the cases.
+
+// let age=22;
+
+// if less than 21->"you can not enter the club!"
+//else          ->"welcome!"
+
+
+
+
+
+
+
+
+
+
+// answer
+// check if the person is old enough to enter the nightclub(21)
+// log a suitable message to the console in both the cases.
+
+let age=22;
+
+// if less than 21->"you can not enter the club!"
+//else          ->"welcome!"
+
+
+
+if(age<21){
+    console.log("you cannot enter in the club!")
+}
+else{
+    console.log("welcome!")
+}
---
+++ if_else_conditionals/question2.js
@@ -0,0 +1,41 @@
+// check if the person is eligible for a birthday card from the king!(100)
+
+// let age=100;
+
+// if less than 100   ->"not eligible"
+// else if exactly 100 ->"Here is your birthday card from the king!"
+// else            -> " Not eligible , you have already gotten one"
+
+
+
+
+
+
+
+
+
+// answer 
+// check if the person is eligible for a birthday card from the king!(100)
+
+
+let age=100;
+
+// if less than 100   ->"not eligible"
+
+if(age<100){
+    console.log("not eligible");
+}
+
+
+// else if exactly 100 ->"Here is your birthday card from the king!"
+
+else if(age===100){
+    console.log("Here is your birthday card from the king!")
+}
+
+
+// else            -> " Not eligible , you have already gotten one"
+else{
+    " Not eligible , you have already gotten one"
+}
+
